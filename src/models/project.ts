99a8254fbe72dfedc422251f658882a101775b7c
Kolotylo Kyrylo <kolot102@gmail.com>
2025-04-14 11:51:03 +0300

    feat(project): add models

---
+++ src/models/project.ts
@@ -0,0 +1,26 @@
+export interface Project {
+    id: number;
+    name: string;
+    description: string;
+    createdAt: Date;
+    status: ProjectStatus;
+    tasks: Task[];
+}
+
+export type ProjectStatus = 'in_progress' | 'completed';
+
+// В завданні не вказано, що потрібно реалізовувати можливість створення нових колонок
+export type ColumnName = "To Do" | "In Progress" | "Done";
+
+export interface Task {
+    id: number;
+    name: string;
+    assigneeName: string;
+    columnName: ColumnName;
+    order: number;
+    status: TaskStatus;
+    projectId: number;
+}
+
+export type TaskStatus = 'in_progress' | 'completed';
+
